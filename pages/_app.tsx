2b74b8a1c52f749c2722714e2cf4992cebc4262c
Lawand Othman <lwnd@pm.me>
2021-05-07 14:16:18 +0100

    install + set up chakra UI (#6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { AuthProvider } from '../lib/auth'
+import customTheme from '../styles/theme'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>
+  <ChakraProvider theme={customTheme}>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  </ChakraProvider>
 )
 
 export default App
