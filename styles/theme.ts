7696f08d4fccf9a916da5bf661e42a0f5223c363
Lawand Othman <lwnd@pm.me>
2021-05-07 14:44:18 +0100

    add global styles

--- styles/theme.ts
+++ styles/theme.ts
@@ -10,6 +10,19 @@ const theme = extendTheme({
     medium: 600,
     bold: 700,
   },
+  styles: {
+    global: {
+      html: {
+        minWidth: '360px',
+        scrollBehavior: 'smooth',
+      },
+      '#__next': {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      },
+    },
+  },
 })
 
 export default theme
