b5743a49d45570ed3305c517a7a81a80e6425e3b
Lawand Othman <lwnd@pm.me>
2021-05-07 14:13:53 +0100

    install + set up chakra UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,31 @@
 import Head from 'next/head'
+import {
+  Button, Heading, Text, Code,
+} from '@chakra-ui/react'
 import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const auth = useAuth()
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fast Feedback</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
-
-        <p className={styles.description}>
-          Get started by editing
-          {' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        <button onClick={() => auth?.signinWithGithub()} type='button'>
-          Sign In
-        </button>
-        <div>{auth?.user?.email}</div>
-        {auth?.user && (
-        <button onClick={() => auth?.signout()} type='button'>
-          Sign Out
-        </button>
+      <main>
+        <Heading>Fast Feedback</Heading>
+        <Text>
+          Current user: <Code>{auth?.user ? auth?.user.email : 'None'}</Code>
+        </Text>
+        {auth?.user ? (
+          <Button onClick={() => auth?.signout()} type='button'>
+            Sign Out
+          </Button>
+        ) : (
+          <Button onClick={() => auth?.signinWithGithub()} type='button'>
+            Sign In
+          </Button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by
-          {' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
