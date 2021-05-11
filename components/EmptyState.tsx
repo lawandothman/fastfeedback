65728e2d58755cfa59b704748ae894abf2949af8
Lawand Othman <lwnd@pm.me>
2021-05-11 15:06:39 +0100

    Add site modal

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {
-  Heading, Text, Button, Flex,
+  Heading, Text, Flex,
 } from '@chakra-ui/react'
 import DashboardShell from './DashboardShell'
+import AddSiteModal from './AddSiteModal'
 
 const EmptyState = () => (
   <DashboardShell>
@@ -17,9 +18,8 @@ const EmptyState = () => (
     >
       <Heading size='lg' mb={2}>You haven&apos;t added any sites.</Heading>
       <Text mb={4}>Let&apos;s get started</Text>
-      <Button fontWeight='medium' maxW='200px' variant='solid' size='md'>
-        Add Your First Site
-      </Button>
+      <AddSiteModal />
+
     </Flex>
   </DashboardShell>
 )
