6d5142a738e91b665306974105c68590049b5eec
Lawand Othman <lwnd@pm.me>
2021-05-14 11:19:01 +0100

    useSwr to fetch sites

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,21 @@
-import { useAuth } from '@/lib/auth'
+import useSwr from 'swr'
+// import { useAuth } from '@/lib/auth'
 import EmptyState from '@/components/EmptyState'
 import SiteTableSkeleton from '@/components/SiteTableSkeleton'
 import DashboardShell from '@/components/DashboardShell'
+import fetcher from '@/util/fetcher'
+import SiteTable from '@/components/SiteTable'
 
 const Dashboard = () => {
-  const auth = useAuth()
+  // const auth = useAuth()
+  const { data } = useSwr('/api/sites', fetcher)
+
+  console.log(data)
 
   return (
     <DashboardShell>
-      {!auth?.user ? <SiteTableSkeleton /> : <EmptyState />}
+      {!data && <SiteTableSkeleton />}
+      {data?.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
     </DashboardShell>
   )
 }
