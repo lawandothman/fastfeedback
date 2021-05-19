910ca9e09764caa82dd205f39fe86f69d32d1b3e
Lawand Othman <lwnd@pm.me>
2021-05-19 12:49:30 +0100

    add IFeedback interface

--- types.ts
+++ types.ts
@@ -13,3 +13,15 @@ export interface ISite {
   name: string
   url: string
 }
+
+export interface IFeedback {
+  id?: string
+  author: string
+  authorId: string
+  createdAt: string
+  provider: string
+  rating: number
+  siteId: string
+  status: string
+  text: string
+}
