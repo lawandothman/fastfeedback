aebdcfed6103ef02bee9b9fb3a5ce6e29b4472d5
Lawand Othman <lwnd@pm.me>
2021-05-19 12:51:18 +0100

    refactor

--- pages/api/sites.ts
+++ pages/api/sites.ts
@@ -1,24 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import db from '@/lib/firebase-admin'
+import admin from '@/lib/firebase-admin'
 import { ISite } from 'types'
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   const sites: ISite[] = []
 
-  const snapshot = await db.collection('sites').get()
-  snapshot.forEach((doc) => {
-    const {
-      authorId, createdAt, name, url,
-    } = doc.data()
+  const snapshot = await admin.firestore().collection('sites').get()
 
-    sites.push({
-      id: doc.id,
-      authorId,
-      createdAt,
-      name,
-      url,
-    })
-  })
+  // @ts-ignore
+  snapshot.forEach((doc) => sites.push({ id: doc.id, ...doc.data() }))
 
   res.status(200).json({ sites })
 }
