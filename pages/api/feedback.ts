e66c0a3c55fb40f29d9863228234aa81064d087a
Lawand Othman <lwnd@pm.me>
2021-05-22 15:56:10 +0100

    feedback api route

---
+++ pages/api/feedback.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getUserFeedback } from '@/lib/firestore-admin'
+import admin from '@/lib/firebase-admin'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.headers.token) {
+    return res.status(401).json({ error: 'Unauthorized' })
+  }
+
+  try {
+    const { uid } = await admin.auth().verifyIdToken(req.headers.token as string)
+    const { feedback } = await getUserFeedback(uid)
+    return res.status(200).json({ feedback })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
