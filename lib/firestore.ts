68272d3328a742d114534041902031d42cfec59f
Lawand Othman <lwnd@pm.me>
2021-05-31 00:03:24 +0100

    createCheckoutSession

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -1,5 +1,6 @@
 import { IFeedback, ISite, IUser } from 'types'
 import firebase from './firebase'
+import getStripe from './stripe'
 
 const firestore = firebase.firestore()
 
@@ -18,3 +19,25 @@ export const createFeedback = (feedback: IFeedback) => {
 }
 
 export const deleteFeedback = (id: string) => firestore.collection('feedback').doc(id).delete()
+
+export const createCheckoutSession = async (uid: string) => {
+  const checkoutSessionRef = await firestore
+    .collection('users')
+    .doc(uid)
+    .collection('checkout_sessions')
+    .add({
+      price: 'price_1IwubBLM94muTW7TuPLrbLHc',
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+    })
+
+  checkoutSessionRef.onSnapshot(async (snap) => {
+    // @ts-ignore
+    const { sessionId } = snap.data()
+
+    if (sessionId) {
+      const stripe = await getStripe()
+      stripe?.redirectToCheckout({ sessionId })
+    }
+  })
+}
