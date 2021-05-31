1a0a5c24791aefe4acbb96f55f1e514f194d32f9
Lawand Othman <lwnd@pm.me>
2021-05-31 15:58:26 +0100

    loading states and view billing portal button

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,16 +1,23 @@
 import DashboardShell from '@/components/DashboardShell'
 import { useAuth } from '@/lib/auth'
-import { createCheckoutSession } from '@/lib/firestore'
+import { createCheckoutSession, goToBillingPortal } from '@/lib/firestore'
 import { Box, Button } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const Account = () => {
   const auth = useAuth()
+  const [isCheckoutLoading, setIsCheckoutLoading] = useState(false)
+  const [isBillingLoading, setIsBillingLoading] = useState(false)
   return (
     <DashboardShell>
       {auth?.user && (
         <Box>
           <Button
-            onClick={() => createCheckoutSession(auth?.user?.uid!)}
+            onClick={() => {
+              setIsCheckoutLoading(true)
+              createCheckoutSession(auth?.user?.uid!)
+            }}
+            isLoading={isCheckoutLoading}
             backgroundColor='gray.900'
             color='white'
             fontWeight='medium'
@@ -21,6 +28,24 @@ const Account = () => {
             }}
           >
             Upgrade to Starter
+          </Button>
+          <Button
+            onClick={() => {
+              setIsBillingLoading(true)
+              goToBillingPortal()
+            }}
+            isLoading={isBillingLoading}
+            backgroundColor='gray.900'
+            color='white'
+            ml={4}
+            fontWeight='medium'
+            _hover={{ bg: 'gray.700' }}
+            _active={{
+              bg: 'gray.800',
+              transform: 'scale(0.95)',
+            }}
+          >
+            View Billing Portal
           </Button>
           <Button ml={4} onClick={() => auth.signout()}>
             Log Out
